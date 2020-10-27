986afa6858a7ba0579a5a695977787b7f0d83e96
vito <gralle89@gmail.com>
2020-10-27 16:00:20 +0100

    landing page, button component and infobox component added

---
+++ glazier-hub/src/App.vue
@@ -0,0 +1,47 @@
+<template>
+  <div id="app">
+    
+<info-box></info-box>
+
+  </div>
+</template>
+
+<script>
+import InfoBox from './components/InfoBox.vue';
+
+export default {
+  name: "App",
+  components: {
+    InfoBox
+  }
+};
+</script>
+
+
+<style lang="scss">
+@import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@500&display=swap');
+
+body,
+html {
+  width: 100%;
+  height: 100%;
+  top: 0;
+  left: 0;
+  overflow: hidden;
+}
+#app {
+  height: 100%;
+  width: 100%;
+  text-align: center;
+  position: fixed;
+  background-image: url("assets/img/bg.png");
+  -webkit-background-size: cover;
+  -moz-background-size: cover;
+  -o-background-size: cover;
+  background-size: cover;
+  overflow: hidden;
+  transform: scale(1.1); 
+  -webkit-font-smoothing: subpixel-antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+</style>
---
+++ glazier-hub/src/components/InfoBox.vue
@@ -0,0 +1,67 @@
+<template>
+  <div class="box">
+    <img src="../assets/img/logo.png" />
+    <h1>Glacier<span style="color: #8dd9ff">Hub</span></h1>
+    <p>
+      We're a consultancy business that specializes in distributed systems and
+      storage solutions.
+      <br /><br />
+      Our team consists of software engineers with years of experience in
+      building <b>scalable</b> and <b>low-latency</b> distributed software.
+    </p>
+    <cta-btn></cta-btn>
+  </div>
+</template>
+
+<script>
+import CtaBtn from "./Button.vue";
+
+export default {
+  name: "info-box",
+  components: {
+    CtaBtn,
+  },
+};
+</script>
+
+<style lang="scss">
+.box {
+  display: flexbox;
+  color: #1d1d1d;
+  background-color: rgba(158, 158, 158, 0.185);
+  backdrop-filter: blur(15px);
+  border-radius: 30px;
+  box-shadow: rgba(114, 114, 114, 0.281) 0px 0px 20px 0px;
+  width: 17em;
+  padding: 15px;
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  -webkit-transform: translate(-50%, -50%);
+  transform: translate(-50%, -50%);
+}
+
+img {
+  width: 25%;
+  height: 18%;
+  margin-bottom: -30px;
+  margin-top: 20px;
+}
+
+h1 {
+  font-family: "Montserrat", sans-serif;
+  color: aliceblue;
+  font-size: 40px;
+  font-weight: bold;
+}
+
+p {
+  font-family: "Verdana", sans-serif;
+  font-size: 12px;
+  opacity: 0.7;
+  color: rgb(243, 243, 243);
+  -webkit-font-smoothing: subpixel-antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  margin-bottom: 10px;
+}
+</style>
