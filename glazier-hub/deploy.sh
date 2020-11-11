c7366e399a0f3e0d642305bff6a723aa97e0c813
vito <gralle89@gmail.com>
2020-11-11 20:46:20 +0100

    adding dist subtree

--- glazier-hub/deploy.sh
+++ glazier-hub/deploy.sh
@@ -20,6 +20,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:grvlle/grvlle.github.io.git main
+git push -f git@github.com:grvlle/glazierhub.git master:gh-pages
 
 cd -
